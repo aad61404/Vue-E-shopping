2b3fb9a1f96ef855d12a9254edd5332744f20358
aad61404 <asd61404@gmail.com>
2019-03-22 09:03:01 +0800

    temporary form

--- src/components/pages/productList.vue
+++ src/components/pages/productList.vue
@@ -166,6 +166,51 @@
 
       </div> <!-- 底下購物車 end -->
 
+   <!-- 訂單資訊 -->
+    <div class="my-5 row justify-content-center">
+      <form class="col-md-6" @submit.prevent="createOrder">
+        <div class="form-group">
+          <label for="useremail">Email</label>
+          <input type="email" class="form-control" name="email" id="useremail" v-model="form.user.email" :class="{'is-invalid': errors.has('email')}"
+            v-validate="'required|email'" placeholder="請輸入 Email"> <!-- required  -->
+          <span class="text-danger" v-if="errors.has('email')">{{ errors.first('email') }}</span>
+        </div>
+
+        <div class="form-group">
+          <label for="username">收件人姓名</label>
+          <input type="text" class="form-control" name="name" id="username" :class="{'is-invalid': errors.has('name')}"
+            v-model="form.user.name" placeholder="輸入姓名" v-validate="'required'">
+          <span class="text-danger" v-if="errors.has('name')">姓名必須輸入</span>
+        </div>
+
+        <div class="form-group">
+          <label for="usertel">收件人電話</label>
+          <input type="tel" class="form-control" id="usertel" name="tel" v-model="form.user.tel" placeholder="請輸入電話"
+            v-validate="'required'">
+          <span class="text-danger" v-if="errors.has('tel')">電話未填</span>
+        </div>
+
+        <div class="form-group">
+          <label for="useraddress">收件人地址</label>
+          <input type="address" class="form-control" name="address" id="useraddress" v-model="form.user.address"
+            v-validate="'required'" placeholder="請輸入地址">
+          <span class="text-danger" v-if="errors.has('address')">地址欄位不得留空</span>
+        </div>
+
+        <div class="form-group">
+          <label for="useraddress">留言</label>
+          <textarea name="message" id="" class="form-control" cols="30" rows="10" v-model="form.message"></textarea>
+        </div>
+        <div class="text-right">
+          <button class="btn btn-danger">送出訂單</button>
+        </div>
+      </form>
+    </div>
+    <!-- 訂單資訊 end -->
+
+
+
+
     </div> <!-- 倒數第二層 -->
 
   </div> <!-- 倒數第一層 -->
